b474099435960754b340ae36b47c0596268987b3
RARES CIUCHINA <raresciuchina01@gmail.com>
2022-04-19 19:00:12 +0200

    Finalizado sin clases y con un error.
    Execution failed for task ':Principal.main()'.
    > Process 'command 'C:/Users/RARES CIUCHINA/.jdks/openjdk-17.0.2/bin/java.exe'' finished with non-zero exit value 1
    
    No consigo dar con ello

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -11,23 +11,19 @@ public class Principal {
 
             int faltan = 10;
             int i;
+            Scanner sc = new Scanner(System.in);
             for(i=0; i<=total; i++) {
                 System.out.println("Dime " + faltan + " numeros enteros");
-                Scanner sc = new Scanner(System.in);
                     array[i] = sc.nextInt();
                     faltan=faltan-1;
             }
-            BuscarNumero(array);
 
-         }
-    private static void BuscarNumero(int[] array) {
 
             Arrays.sort(array); //Ordenamos el array
             int cont = 0;
             int aux= array[0]; //auxiliar almacena el primer valor
 
-        int i;
-        for (i=0; i<=array.length; i++){ //i=0
+            for ( i = 0; i<=9; i++){ //i=0
                 if (aux == array[i])
                 {
                     cont++;
@@ -39,9 +35,16 @@ public class Principal {
                     aux=array[i];
                 }
             }
-                 System.out.println( aux + "  =  " + cont + " veces");
+            System.out.println( aux + "  =  " + cont + " veces");
 
-         }
+
+        }
+          //  BuscarNumero(array);
+
+        // }
+  //  private static void BuscarNumero(int[] array) {
+
+
 
 
 }
