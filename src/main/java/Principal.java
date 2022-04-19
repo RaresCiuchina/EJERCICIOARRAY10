4b62b6ec85c8d251b630f65b258f45f8d7682943
RARES CIUCHINA <raresciuchina01@gmail.com>
2022-04-19 19:01:50 +0200

    Finalizado con una funcion y con un error.
    Execution failed for task ':Principal.main()'.
    > Process 'command 'C:/Users/RARES CIUCHINA/.jdks/openjdk-17.0.2/bin/java.exe'' finished with non-zero exit value 1
    
    No consigo dar con ello

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -17,35 +17,31 @@ public class Principal {
                     array[i] = sc.nextInt();
                     faltan=faltan-1;
             }
-
-
-            Arrays.sort(array); //Ordenamos el array
-            int cont = 0;
-            int aux= array[0]; //auxiliar almacena el primer valor
-
-            for ( i = 0; i<=9; i++){ //i=0
-                if (aux == array[i])
-                {
-                    cont++;
-                }
-                else
-                {
-                    System.out.println( aux + "  =  " + cont + " veces");
-                    cont=1;
-                    aux=array[i];
-                }
-            }
-            System.out.println( aux + "  =  " + cont + " veces");
-
+          BuscarNumero(array);
 
         }
-          //  BuscarNumero(array);
+    private static void BuscarNumero(int[] array) {
 
-        // }
-  //  private static void BuscarNumero(int[] array) {
+        Arrays.sort(array); //Ordenamos el array
+        int cont = 0;
+        int aux= array[0]; //auxiliar almacena el primer valor
+
+        int i;
+        for ( i = 0; i<=9; i++){ //i=0
+            if (aux == array[i])
+            {
+                cont++;
+            }
+            else
+            {
+                System.out.println( aux + "  =  " + cont + " veces");
+                cont=1;
+                aux=array[i];
+            }
+        }
+        System.out.println( aux + "  =  " + cont + " veces");
 
 
-
-
+    }
 }
 
