8bf2808ef2fc59d8c965a2aea3e4b7362029bbd1
RARES CIUCHINA <raresciuchina01@gmail.com>
2022-04-19 18:11:38 +0200

    Rellenamos el array de numeros enteros

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,4 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Principal {
 
+    public static void main (String [] arg){
 
+        int[] array;
+        array = new int [9];
+        final int total = 9 ;
+
+        int faltan = 10;
+        int i;
+        for(i=0; i<=total; i++) {
+            System.out.println("Dime " + faltan + " numeros enteros");
+            Scanner sc = new Scanner(System.in);
+                array[i] = sc.nextInt();
+                faltan=faltan-1;
+        }
+
+
+
+
+    }
 }
